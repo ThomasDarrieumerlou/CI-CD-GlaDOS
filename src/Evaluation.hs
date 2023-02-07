ba6cc43901e547a57756bd56781c13d270985fe1
Lucas Cox <lucas.cox@epitech.eu>
2023-02-07 15:42:47 +0100

    fix(evaluation): fixed default values of operators and use foldl

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -5,7 +5,7 @@
 -- Evaluation.hs
 -}
 
-module Evaluation (evalAst, Bindings) where
+module Evaluation (evalAst, Bindings, getOperator, getNeutral, removeNeutral, extractValue) where
 
 import Ast (
     Operator (Plus, Minus, Times, Div),
@@ -33,8 +33,8 @@ data BuiltinOperator a = BuiltinOperator {
 
 getOperator :: Integral a => Operator -> BuiltinOperator a
 getOperator Plus = BuiltinOperator {function = (+), minArgs = 0, neutral = 0}
-getOperator Minus = BuiltinOperator {function = (-), minArgs = 0, neutral = 0}
-getOperator Times = BuiltinOperator {function = (*), minArgs = 1, neutral = 1}
+getOperator Minus = BuiltinOperator {function = (-), minArgs = 1, neutral = 0}
+getOperator Times = BuiltinOperator {function = (*), minArgs = 0, neutral = 1}
 getOperator Div = BuiltinOperator {function = div, minArgs = 1, neutral = 1}
 
 -- processOperator :: Integral a => BuiltinOperator a -> [Ast] -> Bindings -> Maybe a
@@ -43,11 +43,22 @@ getOperator Div = BuiltinOperator {function = div, minArgs = 1, neutral = 1}
 --   | otherwise = Just (foldr (function op) (neutral op)
 --     (fromJust (mapM (extractValue . fromJust . fst . (`evalAst` bs)) as)))
 
+getNeutral :: Integral a => BuiltinOperator a -> [Ast] -> Bindings -> Maybe a
+getNeutral op as bs
+  | length as < minArgs op = Nothing
+  | length as == minArgs op = Just $ neutral op
+  | otherwise = fst (evalAst (head as) bs) >>= extractValue
+
+removeNeutral :: BuiltinOperator a -> [Ast] -> [Ast]
+removeNeutral op as
+  | length as <= minArgs op = as
+  | otherwise = tail as
+
 processOperator :: Integral a => BuiltinOperator a -> [Ast] -> Bindings -> Maybe a
 processOperator op as bs
   | length as < minArgs op = Nothing
-  | otherwise = Just (foldr (function op) (neutral op)
-    (fromJust (mapM (extractValue . fromJust . fst . (`evalAst` bs)) as)))
+  | otherwise = getNeutral op as bs >>= \n -> Just (foldl (function op) n (fromJust (
+    mapM (extractValue . fromJust . fst . (`evalAst` bs)) (removeNeutral op as))))
 
 extractValue :: Integral a => Ast -> Maybe a
 extractValue (Value (Integer i)) = Just $ fromIntegral i
@@ -72,5 +83,4 @@ evalAst (Define n v) bs = (Nothing, insert n v bs)
 evalAst (Value v) bs = (Just (Value v), bs)
 evalAst (Function p a) bs = (Just (Function p a), bs)
 evalAst (Call n as) bs = (processCall bs n as, bs)
-evalAst (Operator op as) bs = (Just (Value (Integer ((\o ->
-  fromJust $ processOperator o as bs) $ getOperator op))), bs)
+evalAst (Operator op as) bs = (processOperator (getOperator op :: BuiltinOperator Int) as bs >>= \v -> Just (Value (Integer v)), bs)
